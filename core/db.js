5a4844bf1e986f6d3c24ed0a9d78317f0720a130
José Fernando Bellucci Cagale <fernando.b.cagale@gmail.com>
2017-09-28 17:34:49 -0300

    add mongo

---
+++ core/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const uri = process.env.URI || 'mongodb://localhost:27017/challenge';
+
+module.exports = function () {
+  mongoose.connect(uri, { useMongoClient: true });
+
+  mongoose.connection.on('connected', function () {
+    console.log(`Mongoose connected ${uri}`);
+  });
+
+  mongoose.connection.on('disconnected', function () {
+    console.log(`Mongoose disconnected ${uri}`);
+  });
+
+  mongoose.connection.on('error', function (erro) {
+    console.log(`Mongoose error ${erro}`);
+  });
+
+  process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+      console.log(`Mongoose finalized`);
+      process.exit(0);
+    });
+  });
+};
