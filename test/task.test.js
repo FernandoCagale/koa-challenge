0334a46d3d55d593464a4ec28783e3a6c22b4a43
José Fernando Bellucci Cagale <fernando.b.cagale@gmail.com>
2017-09-29 14:24:31 -0300

    add test task

---
+++ test/task.test.js
@@ -0,0 +1,83 @@
+const server = require('../server');
+const request = require('supertest');
+
+let _id;
+
+afterAll(() => {
+  server.close();
+});
+
+describe('Tasks', () => {
+  describe('routes: POST', () => {
+    test('save Task', async () => {
+      const response = await request(server).post('/v1/tasks').send({description: 'web', type: 'development'});
+      expect(response.status).toEqual(201);
+      expect(response.type).toEqual('application/json');
+      expect(response.body.description).toEqual('web');
+      expect(response.body.type).toEqual('development');
+      _id = response.body._id;
+    });
+  });
+
+  describe('routes: GET all', () => {
+    test('all Task', async () => {
+      const response = await request(server).get('/v1/tasks');
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual('application/json');
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body[0].description).toEqual('web');
+      expect(response.body[0].type).toEqual('development');
+      expect(response.body[0]._id).toEqual(_id);
+    });
+  });
+
+  describe('routes: GET id', () => {
+    test('get id Task', async () => {
+      const response = await request(server).get(`/v1/tasks/${_id}`);
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual('application/json');
+      expect(response.body.description).toEqual('web');
+      expect(response.body.type).toEqual('development');
+    });
+
+    test('get id not found', async () => {
+      const response = await request(server).get(`/v1/tasks/59ce75943ffd0b260056b44a`);
+      expect(response.body.output.statusCode).toEqual(404);
+      expect(response.body.output.payload.error).toEqual('Not Found');
+      expect(response.type).toEqual('application/json');
+    });
+  });
+
+  describe('routes: PUT id', () => {
+    test('update id Task', async () => {
+      const response = await request(server).put(`/v1/tasks/${_id}`).send({description: 'database', type: 'dba'});
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual('application/json');
+      expect(response.body.description).toEqual('database');
+      expect(response.body.type).toEqual('dba');
+    });
+
+    test('update id not found', async () => {
+      const response = await request(server).put(`/v1/tasks/59ce75943ffd0b260056b44a`).send({description: 'database', type: 'dba'});
+      expect(response.body.output.statusCode).toEqual(404);
+      expect(response.body.output.payload.error).toEqual('Not Found');
+      expect(response.type).toEqual('application/json');
+    });
+  });
+
+  describe('routes: DELETE id', () => {
+    test('delete id Task', async () => {
+      const response = await request(server).delete(`/v1/tasks/${_id}`);
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual('application/json');
+      expect(response.body.ok).toEqual(true);
+    });
+
+    test('delete id not found', async () => {
+      const response = await request(server).delete(`/v1/tasks/59ce75943ffd0b260056b44a`);
+      expect(response.body.output.statusCode).toEqual(404);
+      expect(response.body.output.payload.error).toEqual('Not Found');
+      expect(response.type).toEqual('application/json');
+    });
+  });
+});
